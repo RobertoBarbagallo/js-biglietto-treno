290dc12d22d92e9a491f67e4a4d04b63d12f6294
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-08 15:47:39 +0200

    aggiunge validazione valori numerici

--- myPlugIn.js
+++ myPlugIn.js
@@ -1,12 +1,21 @@
-var kilometriPercorsi = prompt("Quanti kiometri vuoi percorrere?");
+var kilometriPercorsi = parseInt(prompt("Quanti kiometri vuoi percorrere?"));
 
-var età = prompt ("Quanti anni hai?")
+var età = parseInt(prompt ("Quanti anni hai?"));
 
-if (età < 18){
-    document.getElementById("target").innerHTML= (kilometriPercorsi * 0,21) - ((kilometriPercorsi * 0,21) /100 * 20); 
-} else if (età > 65){
-    document.getElementById("target").innerHTML= (kilometriPercorsi * 0,21) - ((kilometriPercorsi * 0,21) /100 * 40); 
+if (Number.isNaN(kilometriPercorsi) || Number.isNaN(età)){
+    document.getElementById("target").innerHTML= "Hai immasso valori non coerenti"
 }
+
+
 else{
-    document.getElementById("target").innerHTML= (kilometriPercorsi * 0,21);
+    if (età < 18){
+        document.getElementById("target").innerHTML=(kilometriPercorsi * 0,21) - ((kilometriPercorsi * 0,21) /100 * 20) + " Euro"; 
+    } else if (età > 65){
+        document.getElementById("target").innerHTML= (kilometriPercorsi * 0,21) - ((kilometriPercorsi * 0,21) /100 * 40) + " Euro"; 
+    }
+    else{
+        document.getElementById("target").innerHTML=(kilometriPercorsi * 0,21) + " Euro";
+    }
+
 }
+
