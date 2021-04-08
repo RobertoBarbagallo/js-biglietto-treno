d2b356c17ac2687dc78966e0d030ca7348266476
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-08 17:33:37 +0200

    corregge erroraccio - consegna esercizio

--- myPlugIn.js
+++ myPlugIn.js
@@ -1,25 +1,26 @@
 var kilometriPercorsi = parseInt(prompt("Quanti kilometri vuoi percorrere?"));
 
-var età = parseInt(prompt ("Quanti anni hai?"));
+var eta = parseInt(prompt ("Quanti anni hai?"));
 var tariffaBiglietto = (kilometriPercorsi * 0.21);
 var sconto20 = tariffaBiglietto * 20 / 100;
 var sconto40 = tariffaBiglietto * 40 / 100;
 
-if (Number.isNaN(kilometriPercorsi) || Number.isNaN(età)){
+if (Number.isNaN(kilometriPercorsi) || Number.isNaN(eta)){
     document.getElementById("target").innerHTML= "Hai immasso valori non corretti (lettere o simboli)";
 }
-else if (kilometriPercorsi === 0 || età === 0){
+else if (kilometriPercorsi === 0 || eta === 0){
     document.getElementById("target").innerHTML= "I valori immessi non possono essere uguali a Zero";
 }
 
 else{
-    if (età < 18){
-        document.getElementById("target").innerHTML=(kilometriPercorsi * 0,21) - (tariffaBiglietto - sconto20).toFixed(2) + " Euro"; 
-    } else if (età > 65){
-        document.getElementById("target").innerHTML= (kilometriPercorsi * 0,21) - (tariffaBiglietto - sconto40) + " Euro"; 
+    if (eta < 18){
+        document.getElementById("target").innerHTML=
+        (tariffaBiglietto - sconto20).toFixed(2) + " Euro"; 
+    } else if (eta > 65){
+        document.getElementById("target").innerHTML= (tariffaBiglietto - sconto40).toFixed(2)  + " Euro"; 
     }
     else{
-        document.getElementById("target").innerHTML=(kilometriPercorsi * 0,21).toFixed(2) + " Euro";
+        document.getElementById("target").innerHTML= tariffaBiglietto.toFixed(2) + " Euro";
     }
 
 }
