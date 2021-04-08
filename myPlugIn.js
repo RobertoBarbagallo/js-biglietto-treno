0d385f05a2357812b853b8a15f39c76f7448045d
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-08 17:05:10 +0200

    aggiunge variabili

--- myPlugIn.js
+++ myPlugIn.js
@@ -1,6 +1,9 @@
 var kilometriPercorsi = parseInt(prompt("Quanti kiometri vuoi percorrere?"));
 
 var età = parseInt(prompt ("Quanti anni hai?"));
+var tariffaBiglietto = (kilometriPercorsi * 0.21);
+var sconto20 = tariffaBiglietto * 20 / 100;
+var sconto40 = tariffaBiglietto * 40 / 100;
 
 if (Number.isNaN(kilometriPercorsi) || Number.isNaN(età)){
     document.getElementById("target").innerHTML= "Hai immasso valori non corretti (lettere o simboli)";
@@ -11,9 +14,9 @@ else if (kilometriPercorsi === 0 || età === 0){
 
 else{
     if (età < 18){
-        document.getElementById("target").innerHTML=(kilometriPercorsi * 0,21) - ((kilometriPercorsi * 0,21) /100 * 20).toFixed(2) + " Euro"; 
+        document.getElementById("target").innerHTML=(kilometriPercorsi * 0,21) - (tariffaBiglietto - sconto20).toFixed(2) + " Euro"; 
     } else if (età > 65){
-        document.getElementById("target").innerHTML= (kilometriPercorsi * 0,21) - ((kilometriPercorsi * 0,21) /100 * 40).toFixed(2) + " Euro"; 
+        document.getElementById("target").innerHTML= (kilometriPercorsi * 0,21) - (tariffaBiglietto - sconto40) + " Euro"; 
     }
     else{
         document.getElementById("target").innerHTML=(kilometriPercorsi * 0,21).toFixed(2) + " Euro";
